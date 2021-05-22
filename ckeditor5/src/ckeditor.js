2adcd5fdb3954a39fb3e187fbed41a2476ed17a6
Baljitweb <baljitweb@gmail.com>
2021-05-22 22:56:35 +0530

    Add files via upload

---
+++ ckeditor5/src/ckeditor.js
@@ -0,0 +1,23 @@
+/**
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+
+class Editor extends InlineEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Alignment,
+	Autoformat,
+	Essentials,
+	FontColor,
+	Paragraph
+];
+
+export default Editor;
